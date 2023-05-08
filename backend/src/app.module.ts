38d58322ef5dd8d0f42bae511834e5a163a076b5
chamarasab <chamara.sab@outlook.com>
2023-05-08 21:00:29 +0530

    solved prettier problems

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {MongooseModule} from "@nestjs/mongoose";
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-      MongooseModule.forRoot('mongodb+srv://chamara:JK9fDYJPpZJhLZNN@senan.hveg71u.mongodb.net/senan_db?retryWrites=true&w=majority')
+    MongooseModule.forRoot(
+      'mongodb+srv://chamara:JK9fDYJPpZJhLZNN@senan.hveg71u.mongodb.net/senan_db?retryWrites=true&w=majority',
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
