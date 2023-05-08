ad7a2fb711796200e7becc1d9d4e59171d69526f
chamarasab <chamara.sab@outlook.com>
2023-05-08 21:37:36 +0530

    controller created

--- backend/src/students/students.controller.ts
+++ backend/src/students/students.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { StudentsService } from './students.service';
 
 @Controller('students')
-export class StudentsController {}
+export class StudentsController {
+  constructor(private studentsService: StudentsService) {}
+
+  @Get()
+  async getAll() {
+    return this.studentsService.getAll();
+  }
+}
