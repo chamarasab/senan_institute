08947228648125e31cb553ee7c93fc57f8bfc745
chamarasab <chamara.sab@outlook.com>
2023-05-08 20:59:27 +0530

    set db connection string

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
+  imports: [
+      MongooseModule.forRoot('mongodb+srv://chamara:JK9fDYJPpZJhLZNN@senan.hveg71u.mongodb.net/senan_db?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
